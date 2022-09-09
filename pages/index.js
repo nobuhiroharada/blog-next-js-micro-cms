906eaed688805fe5a899eaa2738f32ea63eb3f15
nobu <nobu@rak-3.local>
2022-09-09 16:38:19 +0900

    LayoutコンポーネントをAppコンポーネントに移動

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,5 @@ import { Layout } from 'components/Layout'
 import styles from 'styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <Layout>
-      <Hero />
-    </Layout>
-  )
+  return <Hero />
 }
