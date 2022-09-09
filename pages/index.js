66eb6dca58a5a34ca5459968bf53ea0310c57b1e
nobu <nobu@rak-3.local>
2022-09-09 16:29:04 +0900

    インポートを相対から絶対パスに修正

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Footer } from '../components/footer'
-import { Header } from '../components/Header'
-import { Hero } from '../components/Hero'
-import styles from '../styles/Home.module.css'
+import { Footer } from 'components/footer'
+import { Header } from 'components/Header'
+import { Hero } from 'components/Hero'
+import styles from 'styles/Home.module.css'
 
 export default function Home() {
   return (
