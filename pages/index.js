ea369b7ec20e2d3f4ebfe17a6952628808b204c1
nobu <nobu@rak-3.local>
2022-09-09 16:35:01 +0900

    Layoutコンポーネント追加

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Footer } from 'components/footer'
+import { Footer } from 'components/Footer'
 import { Header } from 'components/Header'
 import { Hero } from 'components/Hero'
+import { Layout } from 'components/Layout'
 import styles from 'styles/Home.module.css'
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-      </main>
-      <Footer />
-    </>
+    <Layout>
+      <Hero />
+    </Layout>
   )
 }
