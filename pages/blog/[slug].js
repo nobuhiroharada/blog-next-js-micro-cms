e5515eab702c5289197699e2105286400e20e930
nobu <nobu@rak-3.local>
2022-09-18 15:32:49 +0900

    DynamicRoute対応

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,4 +1,4 @@
-import { getPostBySlug } from 'lib/api'
+import { getAllSlugs, getPostBySlug } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
 import PostBody from 'components/post-body'
@@ -16,7 +16,7 @@ import Meta from 'components/meta'
 // ローカルのアイキャッチ代替画像
 import { eyecatchLocal } from 'lib/constants'
 
-export default function Schedule({
+export default function Post({
   title,
   publish,
   content,
@@ -63,8 +63,16 @@ export default function Schedule({
   )
 }
 
-export async function getStaticProps() {
-  const slug = 'micro'
+export async function getStaticPaths() {
+  const allSlugs = await getAllSlugs()
+  return {
+    paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(context) {
+  const slug = context.params.slug
   const post = await getPostBySlug(slug)
   const description = extractText(post.content)
   const eyecatch = post.eyecatch ?? eyecatchLocal
