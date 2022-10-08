90cb133b20b00380d32913199ddc1f76ab35a934
nobu <nobu@rak-3.local>
2022-10-08 11:47:25 +0900

    NavのCSS修正

--- components/nav.js
+++ components/nav.js
@@ -17,7 +17,7 @@ export default function Nav() {
 
   return (
     <nav className="w-full bg-white shadow">
-      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+      <div className="justify-between px-5 mx-auto max-w-4xl md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <Link href="/">
