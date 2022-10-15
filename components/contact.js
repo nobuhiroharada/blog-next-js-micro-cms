7f106a3f93dd45ed5b89c3a859e7b44ec88e4e1c
nobu <nobu@rak-3.local>
2022-10-15 13:48:00 +0900

    Footerコンポーネント修正

--- components/contact.js
+++ components/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
     <div className={styles.stack}>
       <h3 className={styles.heading}>Contact</h3>
-      <Social iconSize="30px" />
+      <Social />
       <address>cube@mail.com</address>
     </div>
   )
