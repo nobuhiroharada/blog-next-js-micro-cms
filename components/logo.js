42361cdaa76983500e3476e3c858dc2d203f4f55
nobu <nobu@rak-3.local>
2022-10-22 12:58:32 +0900

    Paginationコンポーネント修正
    その他微修正

--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,10 @@
 import Link from 'next/link'
+import { siteMeta } from 'lib/constants'
 
 export default function Logo() {
   return (
     <Link href="/">
-      <a className="text-2xl font-bold">nobuhiroharada&apos;s blog</a>
+      <a className="text-2xl font-bold">{siteMeta.siteTitle}</a>
     </Link>
   )
 }
