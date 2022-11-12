caa3d314227fca94b86451d4e0005709688586e5
nobu <nobu@rak-3.local>
2022-11-12 10:15:32 +0900

    TwoColumnコンポーネントCSS修正

--- components/two-column.js
+++ components/two-column.js
@@ -1,13 +1,15 @@
-import styles from 'styles/two-column.module.css'
-
 export function TwoColumn({ children }) {
-  return <div className={styles.flexContainer}>{children}</div>
+  return (
+    <div className="flex flex-col md:flex-row md:justify-between gap-12 mt-10 mb-10">
+      {children}
+    </div>
+  )
 }
 
 export function TwoColumnMain({ children }) {
-  return <div className={styles.main}>{children}</div>
+  return <div className="md:w-11/12 ">{children}</div>
 }
 
 export function TwoColumnSidebar({ children }) {
-  return <div className={styles.sidebar}>{children}</div>
+  return <div className="md:w-1/12">{children}</div>
 }
