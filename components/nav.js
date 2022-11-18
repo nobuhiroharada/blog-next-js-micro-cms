8681aca641f883b2900ff1cb7bf5b851ce2982c8
nobu <nobu@rak-3.local>
2022-11-18 20:07:28 +0900

    Improve Header component

--- components/nav.js
+++ components/nav.js
@@ -1,113 +1,91 @@
-import { useRouter } from 'next/router'
 import { useState } from 'react'
-import Link from 'next/link'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { Link as Scroll } from 'react-scroll'
 import { siteMeta } from 'lib/constants'
 
 export default function Nav() {
-  const [navIsOpen, setNavIsOpen] = useState(false)
+  const [activeNav, setActiveNav] = useState('#home')
+  const [nav, setNav] = useState(false)
 
-  const toggleNav = () => {
-    setNavIsOpen((prev) => !prev)
-  }
-
-  const closeNav = () => {
-    setNavIsOpen(false)
-  }
-
-  const router = useRouter()
-
+  const links = [
+    {
+      id: 1,
+      link: 'home',
+    },
+    {
+      id: 2,
+      link: 'about',
+    },
+    {
+      id: 3,
+      link: 'portfolio',
+    },
+    {
+      id: 4,
+      link: 'experience',
+    },
+    {
+      id: 5,
+      link: 'contact',
+    },
+  ]
   return (
-    <nav className="w-full bg-white shadow">
-      <div className="justify-between px-5 mx-auto max-w-4xl md:items-center md:flex">
-        <div>
-          <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link href="/">
-              <a>
-                <h2 className="text-2xl font-bold">{siteMeta.siteTitle}</h2>
-              </a>
-            </Link>
-            <div className="md:hidden">
-              <button
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                onClick={() => toggleNav()}
-              >
-                {navIsOpen ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div
-            className={`flex-1 justify-self-center pb-3 mt-4 md:block md:pb-0 md:mt-0 ${
-              navIsOpen ? 'block' : 'hidden'
-            }`}
-          >
-            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-              <li
-                className={
-                  router.pathname == '/'
-                    ? 'text-gray-400'
-                    : 'hover:text-gray-400'
-                }
-              >
-                <Link href="/">
-                  <a onClick={closeNav}>Home</a>
-                </Link>
-              </li>
-              <li
-                className={
-                  router.pathname == '/about'
-                    ? 'text-gray-400'
-                    : 'hover:text-gray-400'
-                }
-              >
-                <Link href="/about">
-                  <a onClick={closeNav}>About</a>
-                </Link>
-              </li>
-              {/* <li
-                className={
-                  router.pathname == '/blog'
-                    ? 'text-gray-400'
-                    : 'hover:text-gray-400'
-                }
-              >
-                <Link href="/blog">
-                  <a onClick={closeNav}>Blog</a>
-                </Link>
-              </li> */}
-            </ul>
-          </div>
-        </div>
+    <nav className="flex justify-between items-center w-full h-20 px-6 md:px-16 bg-slate-50 text-slate-800 fixed z-50">
+      <div>
+        <h1 className="text-2xl font-bold ml-2">{siteMeta.siteTitle}</h1>
       </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className={
+              activeNav === '#' + link
+                ? 'px-4 cursor-pointer capitalize text-slate-800  hover:text-slate-800 duration-200'
+                : 'px-4 cursor-pointer capitalize text-slate-400  hover:text-slate-800 duration-200'
+            }
+          >
+            <Scroll
+              href={`#${link}`}
+              onClick={() => setActiveNav('#' + link)}
+              to={link}
+              smooth
+              duration={500}
+            >
+              {link}
+            </Scroll>
+          </li>
+        ))}
+      </ul>
+      <div
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        onClick={() => setNav(!nav)}
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-slate-50">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className={
+                activeNav === '#' + link
+                  ? 'px-4 cursor-pointer capitalize py-6 text-4xl text-slate-800'
+                  : 'px-4 cursor-pointer capitalize py-6 text-4xl text-slate-400'
+              }
+            >
+              <Scroll
+                onClick={() => (setNav(!nav), setActiveNav('#' + link))}
+                to={link}
+                smooth
+                duration={500}
+                offset={-80}
+              >
+                {link}
+              </Scroll>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   )
 }
