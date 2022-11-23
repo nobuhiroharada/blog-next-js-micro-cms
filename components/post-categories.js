8dd7d123d1279f84f5ab4e33925219270b95fc7f
nobu <nobu@rak-3.local>
2022-11-23 16:16:35 +0900

    Modify posts list queried by category

--- components/post-categories.js
+++ components/post-categories.js
@@ -5,12 +5,11 @@ export default function PostCategories({ categories }) {
     <div className="text-white mt-4">
       <ul className="flex flex-row gap-2 items-baseline">
         {categories.map(({ name, slug }) => (
-          <li
-            key={slug}
-            className="bg-slate-400 hover:bg-slate-500 px-3 py-2 rounded-md"
-          >
+          <li key={slug}>
             <Link href={`/blog/category/${slug}`}>
-              <a className="">{name}</a>
+              <a className="bg-slate-400 hover:bg-slate-500  rounded-md px-3 py-2">
+                {name}
+              </a>
             </Link>
           </li>
         ))}
